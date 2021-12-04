455af0073c5187ba7b80047b040f53abb0036c20
Charlie Harris <ceharris92@gmail.com>
2021-12-04 18:07:27 +0000

    Day two

--- day_02/positionCalculator.js
+++ day_02/positionCalculator.js
@@ -1,15 +1,32 @@
 const R = require("ramda");
+
 const course = require("./course");
 
-const calculatePos = (pos) => ({
+const calculatePosPartOne = (pos) => ({
   forward: (v) => ({ ...pos, horizontal: pos.horizontal + v }),
   up: (v) => ({ ...pos, depth: pos.depth - v }),
   down: (v) => ({ ...pos, depth: pos.depth + v }),
 });
 
-const { horizontal, depth } = R.reduce(
-  (acc, [command, value]) => calculatePos(acc)[command](value),
-  { horizontal: 0, depth: 0 }
-)(course);
+const calculatePosPartTwo = (pos) => ({
+  forward: (v) => ({
+    ...pos,
+    horizontal: pos.horizontal + v,
+    depth: pos.depth + pos.aim * v,
+  }),
+  up: (v) => ({ ...pos, aim: pos.aim - v }),
+  down: (v) => ({ ...pos, aim: pos.aim + v }),
+});
 
-console.log(horizontal * depth);
+const calculate = (calculatePos) =>
+  R.reduce((acc, [command, value]) => calculatePos(acc)[command](value), {
+    horizontal: 0,
+    depth: 0,
+    aim: 0,
+  })(course);
+
+const partOneResult = calculate(calculatePosPartOne);
+console.log(partOneResult.horizontal * partOneResult.depth);
+
+const partTwoResult = calculate(calculatePosPartTwo);
+console.log(partTwoResult.horizontal * partTwoResult.depth);
