426a13fe806e849849676444cb4cfe56fb347ae8
Charlie Harris <ceharris92@gmail.com>
2021-12-04 17:20:29 +0000

    Day one part two

--- day_01/calculateIncreases.js
+++ day_01/calculateIncreases.js
@@ -1,9 +1,18 @@
 const R = require("ramda");
 const depthMeasurements = require("./depthMeasurements");
 
-const { true: result } = R.compose(
+const { true: partOneResult } = R.compose(
   R.countBy(([prev, curr]) => curr > prev),
   R.aperture(2)
 )(depthMeasurements);
 
-console.log(result);
+console.log("Part one: ", partOneResult);
+
+const { true: partTwoResult } = R.compose(
+  R.countBy(([prev, curr]) => curr > prev),
+  R.aperture(2),
+  R.map(R.sum),
+  R.aperture(3)
+)(depthMeasurements);
+
+console.log("Part two: ", partTwoResult);
