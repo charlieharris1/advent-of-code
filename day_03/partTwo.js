2d7fdb74cc9d2967e0c0625d4b813222d788cb98
Charlie Harris <ceharris92@gmail.com>
2021-12-05 18:52:37 +0000

    Day three part two

--- day_03/partTwo.js
+++ day_03/partTwo.js
@@ -1,43 +1,29 @@
 const R = require("ramda");
-// const report = require("./report");
-
-const report = [
-  "00100",
-  "11110",
-  "10110",
-  "10111",
-  "10101",
-  "01111",
-  "00111",
-  "11100",
-  "10000",
-  "11001",
-  "00010",
-  "01010",
-];
+const report = require("./report");
 
 const countOccurrences = (idx) =>
   R.reduce((acc, value) => (value[idx] ? R.inc(acc) : R.dec(acc)), 0);
 
-const recurse = (occurrenceCounter) => (idx, acc) => {
+const filterByBitOccurrence = (occurrenceFn) => (idx) => (acc) => {
   if (R.length(acc) === 1) return acc;
-  const mostCommonNum = R.compose(occurrenceCounter, countOccurrences(idx))(acc);
+  const num = R.compose(occurrenceFn, countOccurrences(idx))(acc);
 
-  return recurse(occurrenceCounter)(
-    idx + 1,
-    R.filter((value) => value[idx] === mostCommonNum)(acc)
+  return filterByBitOccurrence(occurrenceFn)(idx + 1)(
+    R.filter((value) => value[idx] === num)(acc)
   );
 };
 
-const transformedReport = R.map((r) => Array.from(r, Number))(report);
+const getValue = (occurrenceFn) =>
+  R.compose(
+    (s) => parseInt(s, 2),
+    ([arr]) => arr.join(""),
+    filterByBitOccurrence(occurrenceFn)(0),
+    R.map((r) => Array.from(r, Number))
+  );
 
-const [most] = recurse((v) => (Math.sign(v) === -1 ? 0 : 1))(
-  0,
-  transformedReport
-);
-const [least] = recurse((v) => (Math.sign(v) === -1 ? 1 : 0))(
-  0,
-  transformedReport
-);
+const [most, least] = [
+  (v) => (Math.sign(v) === -1 ? 0 : 1),
+  (v) => (Math.sign(v) === -1 ? 1 : 0),
+].map((fn) => getValue(fn)(report));
 
-console.log(parseInt(R.join("")(most), 2) * parseInt(R.join("")(least), 2));
+console.log(most * least);
